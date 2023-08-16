54bd6057f5ab8a8d62bf816182e2c88aac04e491
James Kotling <3201920+cryptowww@users.noreply.github.com>
2023-08-16 16:00:21 +0800

    test

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"cryptowww.leetcode/algo"
 )
 
 func main() {
-	algo.RotateArrayToRightImplementByTempArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)
-	algo.RotateArrayToRightImplementByReverse([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)
-	algo.RotateArrayToLeftImplementByTempArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)
-	algo.RotateArrayToLeftImplementByReverse([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)
+	fmt.Println(algo.RotateArrayToRightImplementByTempArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3))
+	fmt.Println(algo.RotateArrayToRightImplementByReverse([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3))
+	fmt.Println(algo.RotateArrayToLeftImplementByTempArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3))
+	fmt.Println(algo.RotateArrayToLeftImplementByReverse([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3))
 }
