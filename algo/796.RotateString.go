6b548b8bc36d59a8dd9e6567fd0775a42adc4646
James Kotling <3201920+cryptowww@users.noreply.github.com>
2023-08-29 11:41:11 +0800

    pro.796

---
+++ algo/796.RotateString.go
@@ -0,0 +1,36 @@
+package algo
+
+import "strings"
+
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+func rotateString(s string, goal string) bool {
+	if len(s) != len(goal) {
+		return false
+	}
+	if len(s) == 0 {
+		return true
+	}
+	for i := 0; i < len(s); i++ {
+		if s[i] == goal[0] {
+			j := 0
+			for ; j < len(s); j++ {
+				if s[(i+j)%len(s)] != goal[j] {
+					break
+				}
+			}
+			if j == len(s) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func rotateString2(s string, goal string) bool {
+	if len(s) != len(goal) {
+		return false
+	}
+
+	return strings.Contains(s+s, goal)
+}
